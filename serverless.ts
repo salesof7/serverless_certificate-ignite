cc4ddda3851ab5a752718f32cc4646e7590a4d67
salesof7 <jpsaless2002@gmail.com>
2022-11-03 12:07:14 -0300

    chore: add serverless-offline

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,7 @@ import type { AWS } from "@serverless/typescript";
 const serverlessConfiguration: AWS = {
   service: "certificateignite",
   frameworkVersion: "3",
-  plugins: ["serverless-esbuild"],
+  plugins: ["serverless-esbuild", "serverless-offline"],
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
