394bf65afc8494ee8a44fb2cdac58c788388de19
salesof7 <jpsaless2002@gmail.com>
2022-11-03 12:04:47 -0300

    feat: create the first function

--- serverless.ts
+++ serverless.ts
@@ -19,7 +19,20 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: {},
+  functions: {
+    hello: {
+      handler: "src/functions/hello.handler",
+      events: [
+        {
+          http: {
+            path: "hello",
+            method: "get",
+            cors: true,
+          },
+        },
+      ],
+    },
+  },
   package: { individually: true },
   custom: {
     esbuild: {
