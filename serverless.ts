65fec6563c54f299a99520078b8cc8b41e256e18
salesof7 <jpsaless2002@gmail.com>
2022-11-09 02:37:41 -0300

    chore: configure app to deploy

--- serverless.ts
+++ serverless.ts
@@ -35,7 +35,10 @@ const serverlessConfiguration: AWS = {
       },
     ],
   },
-  // import the function via paths
+  package: {
+    individually: false,
+    include: ["./src/templates/**"],
+  },
   functions: {
     generateCertificate: {
       handler: "src/functions/generateCertificate.handler",
@@ -62,7 +65,6 @@ const serverlessConfiguration: AWS = {
       ],
     },
   },
-  package: { individually: true },
   custom: {
     esbuild: {
       bundle: true,
