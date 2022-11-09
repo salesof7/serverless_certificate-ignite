066e4ca4e165e294087202eaa69e22bb20e18390
salesof7 <jpsaless2002@gmail.com>
2022-11-09 01:21:03 -0300

    feat: ensure a template for the certificate

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,11 +1,31 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbClient";
+import { compile } from "handlebars";
+import { join } from "path";
+import { readFileSync } from "fs";
+import * as dayjs from "dayjs";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate): Promise<string> => {
+  const filePath = join(process.cwd(), "src", "templates", "certificates.hbs");
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   if (event.body === null) {
@@ -44,6 +64,20 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       }),
     };
   }
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal,
+  };
+
+  const content = await compileTemplate(data);
+  console.log(content);
 
   return {
     statusCode: 201,
